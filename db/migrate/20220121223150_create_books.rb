0628fe4447ba2456b009f2a83f8541a6c588074f
daniellemoreno525 <daniellemoreno525@tamu.edu>
2022-02-03 11:32:05 -0600

    Fixing app to run locally first

--- db/migrate/20220121223150_create_books.rb
+++ db/migrate/20220121223150_create_books.rb
@@ -2,9 +2,6 @@ class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
       t.string :title
-      t.string :author
-      t.numerical :price
-      t.date/datetime :publishedDate
 
       t.timestamps
     end
